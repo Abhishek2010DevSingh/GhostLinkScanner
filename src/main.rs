33ae98d3fe2396dd60cb42441b11034ecb92e606
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-02-05 15:07:13 +0530

    using clap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use clap::Parser;
 use ghost_link_scanner::scan::scan_base_url;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 use url::Url;
+
+#[derive(Parser, Debug)]
+#[command(about = "A ghost link scanner for detecting broken links.")]
+struct Args {
+    #[arg(value_parser = Url::parse)]
+    url: Url,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .init();
+    let args = Args::parse();
 
-    scan_base_url(&Url::parse("https://github.com/Abhishek2010DevSingh")?).await
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    info!("Starting scan for URL: {}", args.url);
+
+    scan_base_url(&args.url).await.map_err(|e| {
+        tracing::error!("Scan failed: {:?}", e);
+        e
+    })
 }
